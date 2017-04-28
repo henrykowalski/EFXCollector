14240e6441f19151db97b991666a2787ad9d02ac
Andrzej Palec <andrzej@Andrzejs-MacBook-Pro.local>
2017-04-28 21:12:47 +0200

    vsechny prvky jsme natahali pres Storyboard, pridali dalsi file EFXViewController a nakonec vsechny prvky z teto stranky pretahli ze Storyboardu i do kodu teto stranky

---
+++ EFXCollector/EFXViewController.swift
@@ -0,0 +1,37 @@
+//
+//  EFXViewController.swift
+//  (nick to nazval GameViewController
+//  EFXCollector
+//
+//  Created by Andrzej Palec on 28/04/2017.
+//  Copyright © 2017 Andrzej Palec. All rights reserved.
+//
+
+import UIKit
+
+class EFXViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    // Nick to nazval gameImageView:
+    @IBOutlet weak var efxImageView: UIImageView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func photosTapped(_ sender: Any) {
+    
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
